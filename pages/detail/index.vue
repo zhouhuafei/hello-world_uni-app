e133ad731ad0ec309740c01fdc40eedb077401a7
zhouhuafei <zhouhuafei@shopex.cn>
2018-09-14 18:34:34 +0800

    init

---
+++ pages/detail/index.vue
@@ -0,0 +1,15 @@
+<template>
+	<view class="g-wrap">
+		<view class="iconfont icon-down"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data: {}
+	}
+</script>
+
+<style lang="scss">
+	@import '../../scss/iconfont.scss';
+</style>
---
+++ pages/search/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<view class="g-wrap">
+	</view>
+</template>
+
+<script>
+	export default {
+		data: {}
+	}
+</script>
+
+<style lang="scss">
+</style>
