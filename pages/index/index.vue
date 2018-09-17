0d09133fbf8e9fac0fdb2f3b599e59f89b8e1bca
zhouhuafei <zhouhuafei@shopex.cn>
2018-09-17 16:10:00 +0800

    dist

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<view class="g-wrap">
 		<view>首页</view>
-		<view class="test iconfont icon-down"></view>
+		<view class="iconfont icon-down"></view>
 	</view>
 </template>
 
@@ -13,12 +13,4 @@
 
 <style lang="scss">
 	@import '../../scss/iconfont.scss';
-	.test{
-		flex: 1;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		height: 300px;
-		font-size: 100px;
-	}
 </style>
