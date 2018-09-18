87350ecae4e59eae2fa97da243e38a43d0b1e6bd
zhouhuafei <zhouhuafei@shopex.cn>
2018-09-18 18:23:25 +0800

    静态资源只能存放在static目录里否则会引不到

---
+++ unpackage/dist/dev/mp-weixin/static/pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="g-wrap">
+		<navigator class="g-guide" open-type="navigate" url="/pages/detail/index">
+			<view>去详情页</view>
+			<view class="iconfont icon-right"></view>
+		</navigator>
+		<g-test></g-test>
+		<image src="../../images/cart_selected.png"></image>
+	</view>
+</template>
+
+<script>
+	import gTest from '../../components/g-test';
+	export default {
+		data: {},
+		components: {
+			'g-test': gTest
+		},
+		onLoad: function() {
+			console.log('Index onLoad');
+		},
+		onShow: function() {
+			console.log('Index onShow');
+		},
+		onHide: function() {
+			console.log('Index onHide');
+		}
+	}
+</script>
+
+<style lang="scss"></style>
---
+++ unpackage/dist/dev/mp-weixin/static/pages/search/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="g-wrap">
+		<navigator class="g-guide" open-type="navigate" url="/pages/mine/index">
+			<view>去个人中心页</view>
+			<view class="iconfont icon-right"></view>
+		</navigator>
+	</view>
+</template>
+
+<script>
+	export default {
+		data: {},
+		onLoad: function() {
+			console.log('Search onLoad');
+		},
+		onShow: function() {
+			console.log('Search onShow');
+		},
+		onHide: function() {
+			console.log('Search onHide');
+		}
+	}
+</script>
+
+<style lang="scss"></style>
